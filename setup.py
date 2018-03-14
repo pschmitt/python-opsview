50bec950a92c6c3a2f30042960689782e6a52fc6
Philipp Schmitt <philipp@schmitt.co>
2018-03-14 16:16:58 +0100

    Version 0.9.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def requirements(requirements_file='requirements.txt'):
 
 setup(
     name='opsview',
-    version='0.8.7',
+    version='0.9.0',
     description='Opsview REST API client',
     license='GPLv3',
     long_description=read('README.md'),
